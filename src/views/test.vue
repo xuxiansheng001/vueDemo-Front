ab5e51b7fb40f9e6bc25f9b989bd3fa3c865306c
xuxiansheng001 <xuxianshengdyx@163.com>
2025-07-03 17:56:27 +0800

    testGit

---
+++ src/views/test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Test Page</h1>
+        <p>This is a test page totest demonstrate Vue.js routing.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
